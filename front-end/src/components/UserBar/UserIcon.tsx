064707cbcb2fbfda2c3f5e8ed7b0f496e30ca736
Guyae <guy.apisit2546@gmail.com>
2025-12-19 14:30:01 +0700

    Initial room page

---
+++ front-end/src/components/UserBar/UserIcon.tsx
@@ -0,0 +1,12 @@
+export default function UserIcon() {
+  return (
+    <div className="fixed w-screen">
+      <nav className="relative w-full">
+        <div className="absolute right-5 top-5 bg-blue-400">
+          test1
+          test2
+        </div>
+      </nav>
+    </div>
+  )
+}
