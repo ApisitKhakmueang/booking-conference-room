5db790d29385b92b9fb3ea4e5ec7e90ef16a90d0
Guyae <guy.apisit2546@gmail.com>
2025-12-21 00:12:51 +0700

    Create layout page

--- front-end/src/components/layout/Layout/LayoutPage.tsx
+++ front-end/src/components/layout/Layout/LayoutPage.tsx
@@ -5,21 +5,23 @@ import { useSidebarLayout } from "@/src/context/SidebarLayoutContext";
 type Props = {
   pageName: string;
   children: React.ReactNode;
+  className?: string;
 }
 
 export default function LayoutPage({
   pageName,
   children,
+  className
 }: Props) {
   const { isOpen } = useSidebarLayout()
 
   return (
-    <div className={`flex flex-col py-5 ${isOpen ? 'px-5' : 'px-30'}`}>
-      <h1 className="font-semibold text-4xl">
+    <div className={`flex flex-col py-5 ${isOpen ? 'px-5' : 'md:px-30 px-10'}`}>
+      <h1 className="font-semibold text-4xl pb-5">
         {pageName}
       </h1>
 
-      <div>
+      <div className={className}>
         {children}
       </div>
     </div>
