4906eee95ae5079f366762f323e6cbdc8aeeb052
Guyae <guy.apisit2546@gmail.com>
2026-01-04 15:19:01 +0700

    Separate client side and server side from layout navigation.

--- front-end/src/app/(route)/layout.tsx
+++ front-end/src/app/(route)/layout.tsx
@@ -1,41 +1,13 @@
-"use client";
-
-import { useState } from "react";
-import Sidebar from "@/components/layout/sidebar";
-import UserBar from "@/components/layout/user-bar";
-import useMediaQuery from "@/hooks/ui/useMediaQuery";
-import { useAuth } from "@/hooks/auth/useAuth";
-
-import { SidebarLayoutProvider } from "@/context/SidebarLayoutContext";
+import LayoutNavigation from "@/components/layout/layout-navigation";
 
 export default function SidebarLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  useAuth()
-
-  const isMobile = useMediaQuery("(max-width: 1024px)");
-  const [isOpen, setIsOpen] = useState(true);
-
   return (
-    <SidebarLayoutProvider value={{ isOpen, setIsOpen, isMobile }}>
-      <div className="flex">
-        <Sidebar 
-          isOpen={isOpen} 
-          setIsOpen={setIsOpen} 
-          isMobile={isMobile}/>
-
-        <UserBar 
-          isOpen={isOpen} 
-          isMobile={isMobile}/>
-
-        <main
-          className={`flex-1 transition-duration-300 mt-25 ${!isMobile && (isOpen ? "ml-70" : "ml-23")}
-        `}>
-          {children}
-        </main>
-      </div>
-    </SidebarLayoutProvider>
+    <LayoutNavigation>
+      {children}  
+    </LayoutNavigation>
   )
 }
---
+++ front-end/src/components/layout/layout-navigation.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+// Library
+import { useState } from "react";
+
+// Hook
+import useMediaQuery from "@/hooks/ui/useMediaQuery";
+import { useAuth } from "@/hooks/auth/useAuth";
+
+// Component
+import UserBar from "./user-bar";
+import Sidebar from "./sidebar";
+
+// Store
+import { SidebarLayoutProvider } from "@/context/SidebarLayoutContext";
+
+export default function LayoutNavigation({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  useAuth()
+  
+  const isMobile = useMediaQuery("(max-width: 1024px)");
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <SidebarLayoutProvider value={{ isOpen, setIsOpen, isMobile }}>
+      <div className="flex">
+        <Sidebar 
+          isOpen={isOpen} 
+          setIsOpen={setIsOpen} 
+          isMobile={isMobile}/>
+
+        <UserBar 
+          isOpen={isOpen} 
+          isMobile={isMobile}/>
+
+        <main
+          className={`flex-1 transition-duration-300 mt-25 ${!isMobile && (isOpen ? "ml-70" : "ml-23")}
+        `}>
+          {children}
+        </main>
+      </div>
+    </SidebarLayoutProvider>
+  )
+}
