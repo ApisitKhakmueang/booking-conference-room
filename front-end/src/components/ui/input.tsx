e72193143f8787b34e32b84792ffc35eabd79e9c
Guyae <guy.apisit2546@gmail.com>
2026-01-09 16:21:24 +0700

    Create Input component

---
+++ front-end/src/components/ui/input.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "border border-gray-300 rounded-md p-2",
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+Input.displayName = "Input";
+
+export { Input };
