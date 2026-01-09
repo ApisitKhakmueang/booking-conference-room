2b92be45f241a2581fb20078657286c5f96729cf
Guyae <guy.apisit2546@gmail.com>
2026-01-09 22:40:59 +0700

    Change matcher for skip .gif file

--- front-end/src/proxy.ts
+++ front-end/src/proxy.ts
@@ -8,6 +8,6 @@ export async function proxy(request: NextRequest) {
 // matcher: จะทำงานกับทุกหน้า ยกเว้น _next, favicon, auth/callback, auth/confirm
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|logo|.*\\.(?:svg|png|jpg|jpeg|webp|ico)$|auth/callback|auth/confirm|auth/reset-pass).*)',
+    '/((?!_next/static|_next/image|favicon.ico|logo|.*\\.(?:svg|png|jpg|jpeg|webp|ico|gif)$|auth/callback|auth/confirm|auth/reset-pass).*)',
   ],
 };
