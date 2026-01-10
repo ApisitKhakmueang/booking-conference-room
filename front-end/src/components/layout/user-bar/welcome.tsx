d28ca1d21d74e60a32e8c80d0521caafd14e2546
Guyae <guy.apisit2546@gmail.com>
2026-01-10 15:33:45 +0700

    Set default username

--- front-end/src/components/layout/user-bar/welcome.tsx
+++ front-end/src/components/layout/user-bar/welcome.tsx
@@ -3,7 +3,7 @@ import { WelcombackProps } from "@/lib/interface/interface";
 export default function Welcomeback({ isOpen, user }: WelcombackProps) {
   return (
     <div className={`flex flex-col dark:text-main text-black font-semibold text-3xl ${isOpen ? "pl-5" : "pl-12"}`}>
-      Welcome back, {user?.name}!
+      Welcome back, {user?.name || 'Username'}!
       <span className="font-normal text-base dark:text-secondary text-slate">This is conference room booking system</span>
     </div>
   )
