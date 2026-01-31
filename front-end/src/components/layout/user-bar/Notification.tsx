5423f3e20cf6b234cee5cd3bcfd1cfdf0b68e6af
Guyae <guy.apisit2546@gmail.com>
2026-01-31 21:27:54 +0700

    Change notification from button to div

--- front-end/src/components/layout/user-bar/Notification.tsx
+++ front-end/src/components/layout/user-bar/Notification.tsx
@@ -1,16 +1,15 @@
 import { Bell } from 'lucide-react';
-import Button from '../../ui/button';
 
 export default function NotificationComp() {
   return (
     <div className='flex'>
-      <Button variant="slate" size="circle" className='relative p-3.5 bg-slate-50 text-black hover:bg-slate-200 border border-slate-400 hover:-translate-y-1 transition-duration-300 dark:bg-sidebar dark:text-main dark:border-none dark:hover:bg-hover'>
+      <div className='rounded-full cursor-pointer relative p-3.5 bg-slate-50 text-black hover:bg-slate-200 border border-slate-400 hover:-translate-y-1 transition-duration-300 dark:bg-sidebar dark:text-main dark:border-none dark:hover:bg-hover'>
         <Bell size={24}/>
 
         <div className='absolute top-0 right-0 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full -mt-1 -mr-1'>
           3
         </div>
-      </Button>
+      </div>
     </div>
   )
 }
