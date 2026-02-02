25198e841def94e047e647d864103574dc2d9431
Guyae <guy.apisit2546@gmail.com>
2026-02-03 00:09:49 +0700

    Separate booking router to new file

---
+++ back-end/internal/controller/booking.go
@@ -0,0 +1,18 @@
+package controller
+
+import (
+	"github.com/ApisitKhakmueang/BookingConferenceRoom/internal/http"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func InitialBookingRoute(router fiber.Router, handler *http.OrderHandler) {
+	booking := router.Group("/booking") // สร้าง Group ย่อย /users
+    
+	// กำหนด endpoints
+	booking.Post("/:id", handler.CreateBooking)
+	booking.Get("/:date", handler.GetBooking)
+	booking.Get("/user/:id", handler.GetUserBooking)
+	booking.Put("/:bookingID", handler.UpdateBooking)
+	booking.Delete("/:bookingID", handler.DeleteBooking)
+}
