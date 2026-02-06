bb466c8b02465d5fcd1f13cd1b41a5eab9bf1647
Guyae <guy.apisit2546@gmail.com>
2026-02-07 00:47:34 +0700

    Refactor EachPageLayout to improve sidebar responsiveness and transition effects

--- front-end/src/components/layout/each-page-layout.tsx
+++ front-end/src/components/layout/each-page-layout.tsx
@@ -8,10 +8,15 @@ export default function EachPageLayout({
   children,
   className
 }: EachPageLayoutProps) {
-  const { isOpen } = useSidebarLayout()
+  const { isOpen, isMobile } = useSidebarLayout()
 
   return (
-    <div className={`flex flex-col transition-duration-300 ${isOpen ? 'px-5' : 'lg:px-30 sm:px-10 px-5'}`}>
+    <div className={`flex flex-col transition-[margin,padding] duration-300 
+      ${!isMobile 
+          ? isOpen 
+            ? 'px-5' 
+            : 'lg:px-30 px-5'
+          : "lg:px-30 px-5"}`}>
       <h1 className="font-semibold text-4xl pb-5 dark:text-main">
         {pageName}
       </h1>
