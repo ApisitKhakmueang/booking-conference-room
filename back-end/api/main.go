f6acaf9ba0e36fcb44eee5304ea00fe06afb29d0
Guyae <guy.apisit2546@gmail.com>
2026-02-08 18:51:03 +0700

    Refactor main function to include context and Redis connection, update error handling for .env file

--- back-end/api/main.go
+++ back-end/api/main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"log"
+	"context"
 
 	"github.com/ApisitKhakmueang/BookingConferenceRoom/internal/utils"
 
 	"github.com/joho/godotenv"
 )
 
+var ctx = context.Background()
+
 func main() {
 	if err := godotenv.Load(); err != nil {
-		log.Fatal("Cann't to load env file")
+		// log.Fatal("Can't to load env file")
+		log.Println("Warning: .env file not found, hoping variables are set in environment")
 	}
 
 	db := utils.InitialDBConnection()
@@ -20,9 +24,12 @@ func main() {
 		log.Fatal("Can't connect to google calendar")
 	}
 
-	handleUsecase := utils.InitialCleanArch(db, googleCalendarService)
+	rdb := utils.InitialRedisConnection(ctx)
+	
+	handleUsecase := utils.InitialCleanArch(ctx, rdb, db, googleCalendarService)
 
-	app := utils.InitialFiber(handleUsecase)
+	app := utils.InitialFiber(handleUsecase, rdb, ctx)	
+
 	app.Listen(":8080")
 }
 
