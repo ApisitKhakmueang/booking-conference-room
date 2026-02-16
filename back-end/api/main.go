c77661d8aab9c6aee8da4572b7724949b6e98ca5
Guyae <guy.apisit2546@gmail.com>
2026-02-16 21:57:11 +0700

    Refactor main function to improve signal handling and Redis connection management

--- back-end/api/main.go
+++ back-end/api/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-	"log"
 	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 	_ "time/tzdata" // 🔥 เพิ่มบรรทัดนี้ (ขีดล่าง _ สำคัญมาก)
 
+	"github.com/ApisitKhakmueang/BookingConferenceRoom/internal/delivery/websocket"
 	"github.com/ApisitKhakmueang/BookingConferenceRoom/internal/utils"
 
 	"github.com/joho/godotenv"
@@ -13,6 +17,9 @@ import (
 var ctx = context.Background()
 
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
 	if err := godotenv.Load(); err != nil {
 		// log.Fatal("Can't to load env file")
 		log.Println("Warning: .env file not found, hoping variables are set in environment")
@@ -25,12 +32,35 @@ func main() {
 		log.Fatal("Can't connect to google calendar")
 	}
 
-	rdb := utils.InitialRedisConnection(ctx)
+	rdb, err := utils.InitialRedisConnection(ctx)
+	if err != nil {
+		log.Fatalf("Can't connect to Redis: %v", err)
+	}
+	defer rdb.Close() // ปิด Redis เมื่อจบโปรแกรม
+
+	wsHub := Websocket.NewHub(rdb)
 	
-	handleUsecase := utils.InitialCleanArch(ctx, rdb, db, googleCalendarService)
+	go wsHub.Run(ctx)
 
-	app := utils.InitialFiber(handleUsecase, rdb, ctx)	
+	handleUsecase, websocketHandler := utils.InitialCleanArch(
+		rdb, 
+		db, 
+		googleCalendarService, 
+		wsHub,
+	)
 
-	app.Listen(":8080")
+	app := utils.InitialFiber(handleUsecase, websocketHandler)	
+
+	go func() {
+		if err := app.Listen(":8080"); err != nil {
+			log.Printf("Server Error: %v", err)
+		}
+	}()
+
+	log.Println("Server is running on :8080")
+
+	// 8. Wait for Interrupt Signal (รอจนกว่าจะมีการกด Ctrl+C)
+	<-ctx.Done()
+	log.Println("Shutting down server...")
 }
 
