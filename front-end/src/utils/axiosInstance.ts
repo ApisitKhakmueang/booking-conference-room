e1602f7ec5428b5a1abc36e44f83d5569e03b71b
Guyae <guy.apisit2546@gmail.com>
2026-02-20 23:20:22 +0700

    Rename from lib to utils

---
+++ front-end/src/utils/axiosInstance.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { createClient } from './supabase/client';
+
+const supabase = createClient()
+
+// สร้าง Axios Instance
+const api = axios.create({
+  baseURL: `${process.env.NEXT_PUBLIC_BACKEND_HTTP}`, // ใส่ Base URL ของ Go Backend
+});
+
+// ใช้ Interceptor เพื่อดักจับ Request ก่อนส่งออกไป
+api.interceptors.request.use(
+  async (config) => {
+    // ดึง session ทุกครั้งก่อนยิง API
+    const { data: { session } } = await supabase.auth.getSession();
+    
+    // ถ้ามี session ให้แนบ Token เข้าไปใน Header
+    if (session?.access_token) {
+      config.headers['Authorization'] = `Bearer ${session.access_token}`;
+    }
+    
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
