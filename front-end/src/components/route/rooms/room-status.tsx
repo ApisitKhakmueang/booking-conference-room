a33a00e279a429d076f0f8cd84193ae36059c3a2
Guyae <guy.apisit2546@gmail.com>
2026-03-13 00:59:26 +0700

    feature: count each status, and add CardVariant for confirm that passed value is CardVariant

--- front-end/src/components/route/rooms/room-status.tsx
+++ front-end/src/components/route/rooms/room-status.tsx
@@ -1,4 +1,8 @@
-import Card from "@/components/ui/card"
+'use client'
+
+import Card, { CardVariant } from "@/components/ui/card"
+import { RoomResp } from "@/utils/interface/response"
+import { useMemo } from "react"
 
 const ROOM_STATUS = [
   { name: 'Total', amount: 10, variant: 'dark-purple' },
@@ -7,12 +11,26 @@ const ROOM_STATUS = [
   { name: 'Maintainance', amount: 1, variant: 'purple' },
 ] as const
 
-export default function RoomStatus() {
+export default function RoomStatus({ displayRooms }: { displayRooms: RoomResp[] }) {
+  const countAmountRoom = useMemo(() => {
+    const total = displayRooms.length
+    const available = displayRooms.filter((room) => room.status === 'available').length
+    const occupied = displayRooms.filter((room) => room.status === 'occupied').length
+    const maintenance = displayRooms.filter((room) => room.status === 'maintenance').length
+
+    return  [
+      { name: 'Total', amount: total, variant: 'dark-purple' },
+      { name: 'Available', amount: available, variant: 'purple' },
+      { name: 'Occupied', amount: occupied, variant: 'purple' },
+      { name: 'Maintainance', amount: maintenance, variant: 'purple' },
+    ]
+  }, [displayRooms])
+
   return (
     <>
       <ul className="grid md:grid-cols-4 grid-cols-2 gap-6">
-        {ROOM_STATUS.map((item) => (
-          <Card key={item.name} variant={item.variant} loading={false}>
+        {countAmountRoom.map((item) => (
+          <Card key={item.name} variant={item.variant as CardVariant} loading={false}>
             <li className='flex flex-col gap-5 xl:p-6 p-3'>
               <h1 className={`text-start font-semibold xl:text-3xl sm:text-2xl text-lg`}>
                 {item.name}
