fdcf11b4c5b32b05a7098fb9154b9d6e9e7cbade
Guyae <guy.apisit2546@gmail.com>
2026-03-22 23:58:32 +0700

    style: create new condition for schedule file

--- front-end/src/components/layout/each-page-layout.tsx
+++ front-end/src/components/layout/each-page-layout.tsx
@@ -23,7 +23,7 @@ export default function EachPageLayout({
             ? 'px-5' 
             : 'lg:px-30 px-5'
           : "lg:px-30 px-5"}`}>
-      <h1 className="font-semibold text-4xl pb-5 dark:text-main">
+      <h1 className={`font-semibold text-4xl dark:text-main ${pageName === "Schedule" ? 'pb-2' : ' pb-5'}`}>
         {pageName}
       </h1>
 
