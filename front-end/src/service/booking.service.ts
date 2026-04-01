5891aee1aa2e4ddcc960729bf777de48dd8070ab
Guyae <guy.apisit2546@gmail.com>
2026-04-01 17:26:02 +0700

    refactor: replace axios with custom api instance in booking service

--- front-end/src/service/booking.service.ts
+++ front-end/src/service/booking.service.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import api from '@/utils/axiosInstance';
 
 const API_URL = process.env.NEXT_PUBLIC_BACKEND_HTTP;
 const USER_ID = "6d4ac759-dd57-4462-b980-4147b7d18cba"; // ในอนาคตควรดึงจาก Auth context
@@ -6,31 +6,31 @@ const USER_ID = "6d4ac759-dd57-4462-b980-4147b7d18cba"; // ในอนาคต
 export const bookingService = {
   // ดึงข้อมูลการจองตามวันที่
   fetchUserBookings: async (date: string) => {
-    const response = await axios.get(`${API_URL}/booking/user/${USER_ID}?date=${date}`);
+    const response = await api.get(`${API_URL}/booking/user/${USER_ID}?date=${date}`);
     return response.data;
   },
 
   fetchUserHistory: async (date: string) => {
-    const response = await axios.get(`${API_URL}/booking/history/${USER_ID}?date=${date}`);
+    const response = await api.get(`${API_URL}/booking/history/${USER_ID}?date=${date}`);
     return response.data;
   },
 
   fetchHolidays: async (startYear:string, endYear: string) => {
-    const response = await axios.get(`${API_URL}/holiday?startDate=${startYear}&endDate=${endYear}`)
+    const response = await api.get(`${API_URL}/holiday?startDate=${startYear}&endDate=${endYear}`)
     return response.data
   },
 
   // สร้างการจองใหม่
   createBooking: async (roomNumber: number | undefined, body: any) => {
-    return await axios.post(`${API_URL}/booking/${USER_ID}?room=${roomNumber}`, body);
+    return await api.post(`${API_URL}/booking/${USER_ID}?room=${roomNumber}`, body);
   },
 
   updateBooking: async (bookingID: string | undefined, roomNumber: number | undefined, body: any) => {
-    return await axios.put(`${API_URL}/booking/${bookingID}?room=${roomNumber}`, body)
+    return await api.put(`${API_URL}/booking/${bookingID}?room=${roomNumber}`, body)
   },
 
   deleteBooking: async (bookingID: string | undefined) => {
-    return await axios.delete(`${API_URL}/booking/${bookingID}`)
+    return await api.delete(`${API_URL}/booking/${bookingID}`)
   }
   
   // เพิ่ม action อื่นๆ เช่น update, delete ได้ที่นี่
