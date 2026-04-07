fec43955e05cd62a6c2211075e85e1aa298b07b4
Guyae <guy.apisit2546@gmail.com>
2026-04-07 23:03:51 +0700

    refactor: use middleware to getBookingOneDay route

--- back-end/internal/controller/bookingRoute.go
+++ back-end/internal/controller/bookingRoute.go
@@ -10,18 +10,18 @@ func InitialBookingRoute(router fiber.Router, handler *http.BookingHandler) {
 	router.Post("/", handler.CreateBooking)             // POST   /bookings/
 	router.Put("/:bookingID", handler.UpdateBooking)    // PUT    /bookings/:id
 	router.Delete("/:bookingID", handler.DeleteBooking) // DELETE /bookings/:id (ลบคำว่า /delete ออก)
-
+	
 	// Action เฉพาะกิจ (อิงตาม ID)
 	router.Patch("/:bookingID/checkout", handler.CheckOutBooking) // PATCH /bookings/:id/checkout (เปลี่ยนมาใช้ Patch/Post)
 	
 	// ข้อมูลส่วนตัวของคนที่ Login
+	router.Get("/oneDay/:date", handler.GetBookingOneDay)
 	router.Get("/me", handler.GetUserBooking)           // GET /bookings/me
 	router.Get("/me/history", handler.GetUserHistory)   // GET /bookings/me/history
 }
 
 func InitialHelperRoute(router fiber.Router, handler *http.BookingHandler) {
 	// มาตรฐานมักจะใช้เป็นพหูพจน์ (Plural)
-	router.Get("/oneDay/:date", handler.GetBookingOneDay)
 	router.Post("/room/:roomID/checkin", handler.CheckInBooking)  // POST  /bookings/room/:id/checkin
 	router.Get("/holidays", handler.GetHoliday) 
 	router.Get("/rooms/details", handler.GetRoomDetails) 
