5dd58d6e8b497f2712502ec5f58ad2930608f30c
Guyae <guy.apisit2546@gmail.com>
2026-04-27 22:30:55 +0700

    feat: add booking and websocket route handlers for improved functionality

---
+++ back-end/internal/controller/handler_route.go
@@ -0,0 +1,46 @@
+package controller
+
+import (
+	"github.com/ApisitKhakmueang/BookingConferenceRoom/internal/delivery/http"
+	"github.com/gofiber/fiber/v2"
+)
+
+func InitialBookingRoute(router fiber.Router, handler *http.BookingHandlers) {
+	// CRUD พื้นฐาน
+	router.Post("/room/:roomNumber", handler.CreateBooking)           // POST   /bookings/
+	router.Put("/:bookingID/room/:roomNumber", handler.UpdateBooking) // PUT    /bookings/:id
+	router.Delete("/:bookingID", handler.DeleteBooking)               // DELETE /bookings/:id (ลบคำว่า /delete ออก)
+
+	// Action เฉพาะกิจ (อิงตาม ID)
+	router.Patch("/:bookingID/checkout", handler.CheckOutBooking) // PATCH /bookings/:id/checkout (เปลี่ยนมาใช้ Patch/Post)
+
+	// ข้อมูลส่วนตัวของคนที่ Login
+	router.Get("/date/:date", handler.GetBookingByDay)
+	router.Get("/up-next/:date", handler.GetUpNextBooking)
+	router.Get("/analytic/startDate/:startDate/endDate/:endDate", handler.GetAnalyticBooking)
+	router.Get("/me/date/:date", handler.GetUserBooking)         // GET /bookings/me
+	router.Get("/me/history/date/:date", handler.GetUserHistory) // GET /bookings/me/history
+}
+
+func InitialRoomRoute(router fiber.Router, handler *http.RoomHandler) {
+	router.Post("/", handler.CreateRoom)
+	router.Put("/:roomID", handler.UpdateRoom)
+	router.Delete("/:roomID", handler.DeleteRoom)
+}
+
+func InitialHelperRoute(router fiber.Router, bookingHandler *http.BookingHandlers, roomHandler *http.RoomHandler, configHandler *http.ConfigHandler) {
+	// มาตรฐานมักจะใช้เป็นพหูพจน์ (Plural)
+	router.Get("/config", configHandler.GetConfig)
+	router.Post("/room/:roomID/checkin", bookingHandler.CheckInBooking) // POST  /bookings/room/:id/checkin
+	router.Get("/holidays/startDate/:startDate/endDate/:endDate", configHandler.GetHoliday)
+	router.Get("/rooms/details", roomHandler.GetRoom)
+	router.Get("/room/:roomID", roomHandler.GetRoomByID) // GET   /bookings/room/:id
+}
+
+func InitialAdminRoute(router fiber.Router, userHandler *http.UserHandler, configHandler *http.ConfigHandler) {
+	router.Put("/config", configHandler.UpdateConfig)
+	router.Get("/users", userHandler.GetPaginatedUsers)
+	router.Get("/users/:userID/overview", userHandler.GetUserOverview)
+	router.Get("/users/:userID/bookings", userHandler.GetPaginatedUserBookings)
+	router.Put("/users/:userID", userHandler.UpdateUserStatus)
+}
--- back-end/internal/controller/websocket_route.go
+++ back-end/internal/controller/websocket_route.go
@@ -1,10 +1,8 @@
 package controller
 
 import (
-	// "github.com/ApisitKhakmueang/BookingConferenceRoom/internal/delivery/http"
 	"github.com/ApisitKhakmueang/BookingConferenceRoom/internal/delivery/websocket"
 	"github.com/ApisitKhakmueang/BookingConferenceRoom/internal/utils/middleware"
-	// "github.com/ApisitKhakmueang/BookingConferenceRoom/internal/utils/middleware"
 	"github.com/nedpals/supabase-go"
 
 	"github.com/gofiber/fiber/v2"
@@ -16,10 +14,4 @@ func InitialWSRoute(router fiber.Router, ws *Websocket.WSBookingHandler, supabas
 	router.Get("/room/:roomID", websocket.New(ws.GetBookingStatusByRoomID))
 	router.Get("/status", websocket.New(middleware.WithWSAuth(supabaseClient, ws.GetBookingStatus)))
 	router.Get("/:room", websocket.New(middleware.WithWSAuth(supabaseClient, ws.GetBooking)))
-	
-	// For test without middleware
-	// router.Get("/:room", websocket.New(ws.GetBooking))
-	// router.Get("/status", websocket.New(ws.GetBookingStatus))
-	// router.Get("/:room", handler.GetBooking)
-	// router.Get("/user/:id", handler.GetUserBooking)
 }
