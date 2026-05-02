06f98adef606c9833c3959895d8f8207135068b0
Guyae <guy.apisit2546@gmail.com>
2026-05-02 16:35:07 +0700

    refactor: response api with json patthern

--- back-end/internal/delivery/http/booking_handler.go
+++ back-end/internal/delivery/http/booking_handler.go
@@ -21,116 +21,158 @@ func (u *BookingHandlers) CreateBooking(c *fiber.Ctx) error {
 	userID, err := uuid.Parse(c.Locals("user_id").(string))
 	// userID, err := uuid.Parse(c.Params("userID"))
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+    	"message": err.Error(),
+		})
 	}
 
 	roomNumber, err := strconv.Atoi(c.Params("roomNumber"))
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+    	"message": err.Error(),
+		})
 	}
 	
 	booking := new(domain.Booking)
 	if err := c.BodyParser(booking); err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+    	"message": err.Error(),
+		})
 	}
 
 	booking.UserID = userID
 
 	if err := u.usecase.CreateBooking(ctx, booking, uint(roomNumber)) ; err != nil {
-		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+    	"message": err.Error(),
+		})
 	}
 
-	return c.Status(fiber.StatusCreated).SendString("Create booking successfully !")
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"message": "Create booking successfully !",
+	})
 }
 
 func (u *BookingHandlers) UpdateBooking(c *fiber.Ctx) error {
 	ctx := c.UserContext()
 	userID, err := uuid.Parse(c.Locals("user_id").(string))
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+    	"message": err.Error(),
+		})
 	}
 
 	bookingID, err := uuid.Parse(c.Params("bookingID"))
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+    	"message": err.Error(),
+		})
 	}
 
 	// ดึงจาก Query
 	roomNumber, err := strconv.Atoi(c.Params("roomNumber"))
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+    	"message": err.Error(),
+		})
 	}
 
 	if roomNumber <= 0 {
-		return c.Status(fiber.StatusBadRequest).SendString("Please send room number")
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Please send room number",
+		})
 	}
 
 	booking := new(domain.Booking)
 	if err := c.BodyParser(booking); err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+    	"message": err.Error(),
+		})
 	}
 
 	booking.ID = bookingID
 	booking.UserID = userID
 
 	if err := u.usecase.UpdateBooking(ctx, booking, uint(roomNumber)); err != nil {
-		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+    	"message": err.Error(),
+		})
 	} 
 
-	return c.Status(fiber.StatusOK).SendString("Update book successfully")
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "Update book successfully",
+	})
 }
 
 func (u *BookingHandlers) DeleteBooking(c *fiber.Ctx) error {
 	ctx := c.UserContext()
 	userID, err := uuid.Parse(c.Locals("user_id").(string))
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+    	"message": err.Error(),
+		})
 	}
 
 	booking := new(domain.Booking)
 	bookingID, err := uuid.Parse(c.Params("bookingID"))
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+    	"message": err.Error(),
+		})
 	}
 
 	booking.ID = bookingID
 	booking.UserID = userID
 
 	if err := u.usecase.DeleteBooking(ctx, booking) ; err != nil {
-		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+    	"message": err.Error(),
+		})
 	}
 
-	return c.Status(fiber.StatusOK).SendString("Delete booking successfully !")
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "Delete booking successfully !",
+	})
 }
 
 func (u *BookingHandlers) CheckOutBooking(c *fiber.Ctx) error {
 	ctx := c.UserContext()
 	userID, err := uuid.Parse(c.Locals("user_id").(string))
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+    	"message": err.Error(),
+		})
 	}
 
 	booking := new(domain.Booking)
 	bookingID, err := uuid.Parse(c.Params("bookingID"))
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error()) 
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+    	"message": err.Error(),
+		}) 
 	}
 
 	booking.ID = bookingID
 	booking.UserID = userID
 
 	if err := u.usecase.CheckOutBooking(ctx, booking) ; err != nil {
-		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+    	"message": err.Error(),
+		})
 	}
 
-	return c.Status(fiber.StatusOK).SendString("Checkout booking successfully !")
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "Checkout booking successfully !",
+	})
 }
 
 func (u *BookingHandlers) CheckInBooking(c *fiber.Ctx) error {
 	ctx := c.UserContext()
 	roomID, err := uuid.Parse(c.Params("roomID"))
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+    	"message": err.Error(),
+		})
 	}
 
 	// ⭐️ สร้าง Struct เฉพาะกิจมารับ JSON แค่ฟิลด์เดียว
@@ -139,19 +181,27 @@ func (u *BookingHandlers) CheckInBooking(c *fiber.Ctx) error {
 	}
 	
 	if err := c.BodyParser(&req); err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+    	"message": err.Error(),
+		})
 	}
 
 	if req.Passcode == "" {
-		return c.Status(fiber.StatusBadRequest).SendString("Passcode is required")
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Passcode is required",
+		})
 	}
 
 	// ส่ง req.Passcode (ที่เป็น string ธรรมดา) ไปได้เลย ปลอดภัย 100%
 	if err := u.usecase.CheckInBooking(ctx, roomID, req.Passcode); err != nil {
-		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+    	"message": err.Error(),
+		})
 	}
 
-	return c.Status(fiber.StatusOK).SendString("Checkin booking successfully!")
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "Checkin booking successfully!",
+	})
 }
 
 func (u *BookingHandlers) GetBookingByDay(c *fiber.Ctx) error {
@@ -160,7 +210,9 @@ func (u *BookingHandlers) GetBookingByDay(c *fiber.Ctx) error {
 
 	bookings, err := u.usecase.GetBookingByDay(ctx, date)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+    	"message": err.Error(),
+		})
 	}
 
 	return c.Status(fiber.StatusOK).JSON(bookings)
@@ -172,7 +224,9 @@ func (u *BookingHandlers) GetUpNextBooking(c *fiber.Ctx) error {
 
 	booking, err := u.usecase.GetUpNextBooking(ctx, date)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+    	"message": err.Error(),
+		})
 	}
 
 	return c.Status(fiber.StatusOK).JSON(booking)
@@ -186,12 +240,16 @@ func (u *BookingHandlers) GetAnalyticBooking(c *fiber.Ctx) error {
 	// 3. สั่ง Parser (มันจะทับค่า Default เฉพาะตัวที่ส่งมาถูกต้อง)
 	// ถ้าส่ง ?year=-5 มันจะ Parse ไม่ผ่าน และใช้ค่า Default (หรือเป็น 0) ให้เอง
 	if err := c.ParamsParser(params); err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+    	"message": err.Error(),
+		})
 	}
 
 	response, err := u.usecase.GetAnalyticBooking(ctx, params)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+    	"message": err.Error(),
+		})
 	}
 	// log.Printf("res: %v", response)
 
@@ -202,14 +260,18 @@ func (u *BookingHandlers) GetUserBooking(c *fiber.Ctx) error {
 	ctx := c.UserContext()
 	userID, err := uuid.Parse(c.Locals("user_id").(string))
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+    	"message": err.Error(),
+		})
 	}
 
 	date := c.Params("date")
 
 	bookings, err := u.usecase.GetUserBooking(ctx, userID, date)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+    	"message": err.Error(),
+		})
 	}
 
 	return c.Status(fiber.StatusOK).JSON(bookings)
@@ -219,14 +281,18 @@ func (u *BookingHandlers) GetUserHistory(c *fiber.Ctx) error {
 	ctx := c.UserContext()
 	userID, err := uuid.Parse(c.Locals("user_id").(string))
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+    	"message": err.Error(),
+		})
 	}
 
 	date := c.Params("date")
 
 	bookings, err := u.usecase.GetUserHistory(ctx, userID, date)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+    	"message": err.Error(),
+		})
 	}
 
 	return c.Status(fiber.StatusOK).JSON(bookings)
