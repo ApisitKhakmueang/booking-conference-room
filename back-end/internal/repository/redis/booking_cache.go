7e39fe06fe19ccc79315f653a0a2651a34b9403d
Guyae <guy.apisit2546@gmail.com>
2026-05-02 15:03:34 +0700

    refactor: remove ClearCacheByPrefix from booking_cache.go

--- back-end/internal/repository/redis/booking_cache.go
+++ back-end/internal/repository/redis/booking_cache.go
@@ -1,8 +1,6 @@
 package redisRepo
 
 import (
-	"context"
-
 	"github.com/ApisitKhakmueang/BookingConferenceRoom/internal/domain"
 	"github.com/redis/go-redis/v9"
 )
@@ -15,30 +13,4 @@ func NewBookingRedisRepo(rdb *redis.Client) domain.BookingRedisRepo {
 	return &bookingRedisRepo{
 		BaseRedisRepo: &BaseRedisRepo{rdb:rdb},
 	}
-}
-
-// Internal Function
-func (r *bookingRedisRepo) ClearCacheByPrefix(ctx context.Context,prefix string) error {
-	var keys []string
-
-	// วนลูปหา Key ที่ขึ้นต้นด้วย prefix (เช่น "holidays:*")
-	// เราใช้ Scan แทน Keys เพราะ Keys จะทำให้ Redis ค้างถ้าข้อมูลเยอะ
-	iter := r.rdb.Scan(ctx, 0, prefix+"*", 0).Iterator()
-	
-	for iter.Next(ctx) {
-		keys = append(keys, iter.Val())
-	}
-
-	if err := iter.Err(); err != nil {
-		return err
-	}
-
-	// ถ้าเจอ Key ก็สั่งลบทีเดียว
-	if len(keys) > 0 {
-		if err := r.rdb.Del(ctx, keys...).Err(); err != nil {
-			return err
-		}
-	}
-
-	return nil
 }
