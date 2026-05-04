690f0a6ea7f73dccd6144c6d409c2dc9a6af54d6
Guyae <guy.apisit2546@gmail.com>
2026-05-04 23:22:21 +0700

    chore: install @testing-library/user-event library

--- front-end/vitest.config.ts
+++ front-end/vitest.config.ts
@@ -4,6 +4,8 @@ import tsconfigPaths from 'vite-tsconfig-paths' // 🌟 1. Import plugin เข�
 export default defineConfig({
   plugins: [tsconfigPaths()], // 🌟 2. เรียกใช้งาน Plugin ตรงนี้
   test: {
+    globals: true, // 🌟 สำคัญมาก ต้องเปิดเป็น true
     environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'], // 👈 ชี้ไปที่ไฟล์ที่เราสร้างในข้อ 2
   },
 })
